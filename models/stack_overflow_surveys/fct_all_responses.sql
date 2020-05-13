40893e2e7ad7c40089943c8da969001ed72db057
Ubuntu <ubuntu@ip-172-31-19-239.us-west-2.compute.internal>
2020-05-13 16:21:51 +0000

    Add logic for normalizing year responses

--- models/stack_overflow_surveys/fct_all_responses.sql
+++ models/stack_overflow_surveys/fct_all_responses.sql
@@ -1,40 +1,22 @@
 {{ config(materialized='table') }}
 
-/*
-DEMOS
-X titles
-location
 
-TOOLS
-IDE
-languages
-database worked with
-
-EMPLOYMENT
-company size
-years of experience
-salary
-education level
-employment status
-
-
-*/
-
-
-with responses_2016 as (
-select 
-    2016 as year,
-    col_0_unnamed as respondent,
-    coalesce(regexp_split_to_array(replace(self_identification, ' ', ''), E';'), ARRAY['Unset']::text[]) as titles
-    -- age_midpoint as age
-from survey_2016_responses
-),
-
-responses_2017 as (
+with responses_2017 as (
 select
     2017 as year,
     respondent,
-    coalesce(regexp_split_to_array(replace(developer_type, ' ', ''), E';'), ARRAY['Unset']::text[]) as titles
+    country,
+    company_size,
+    coalesce(regexp_split_to_array(replace(developer_type, ' ', ''), E';'), ARRAY['Unset']::text[]) as titles,
+    years_coded_job as years_professional_experience,
+    salary as annual_salary_usd,
+    formal_education as education_level,
+    employment_status,
+    coalesce(regexp_split_to_array(i_d_e, E';'), ARRAY['Unset']::text[]) as development_environments,
+    coalesce(regexp_split_to_array(have_worked_language, E';'), ARRAY['Unset']::text[]) as development_languages_used,
+    coalesce(regexp_split_to_array(want_work_language, E';'), ARRAY['Unset']::text[]) as development_languages_wanted,
+    coalesce(regexp_split_to_array(have_worked_database, E';'), ARRAY['Unset']::text[]) as databases_used,
+    coalesce(regexp_split_to_array(want_work_database, E';'), ARRAY['Unset']::text[]) as databases_wanted
 from survey_2017_responses
 ),
 
@@ -42,7 +24,18 @@ responses_2018 as (
 select
     2018 as year,
     respondent,
-    coalesce(regexp_split_to_array(replace(dev_type, ' ', ''), E';'), ARRAY['Unset']::text[]) as titles
+    country,
+    company_size,
+    coalesce(regexp_split_to_array(dev_type, E';'), ARRAY['Unset']::text[]) as titles,
+    years_coding_prof as years_professional_experience,
+    converted_salary as annual_salary_usd,
+    formal_education as education_level,
+    employment as employment_status,
+    coalesce(regexp_split_to_array(i_d_e, E';'), ARRAY['Unset']::text[]) as development_environments,
+    coalesce(regexp_split_to_array(language_worked_with, E';'), ARRAY['Unset']::text[])  as development_languages_used,
+    coalesce(regexp_split_to_array(language_desire_next_year, E';'), ARRAY['Unset']::text[])  as development_languages_wanted,
+    coalesce(regexp_split_to_array(database_worked_with, E';'), ARRAY['Unset']::text[])  as databases_used,
+    coalesce(regexp_split_to_array(database_desire_next_year, E';'), ARRAY['Unset']::text[])  as databases_wanted
 from survey_2018_responses
 ),
 
@@ -50,15 +43,35 @@ responses_2019 as (
 select
     2019 as year,
     respondent,
-    coalesce(regexp_split_to_array(dev_type, E';'), ARRAY['Unset']::text[]) as titles
-    -- age,
+    country,
+    org_size as company_size,
+    coalesce(regexp_split_to_array(dev_type, E';'), ARRAY['Unset']::text[]) as titles,
+    years_code_pro as years_professional_experience,
+    converted_comp as annual_salary_usd,
+    ed_level as education_level,
+    employment as employment_status,
+    coalesce(regexp_split_to_array(dev_environ, E';'), ARRAY['Unset']::text[])  as development_environments,
+    coalesce(regexp_split_to_array(language_worked_with, E';'), ARRAY['Unset']::text[]) as development_languages_used,
+    coalesce(regexp_split_to_array(language_desire_next_year, E';'), ARRAY['Unset']::text[]) as development_languages_wanted,
+    coalesce(regexp_split_to_array(database_worked_with, E';'), ARRAY['Unset']::text[]) as databases_used,
+    coalesce(regexp_split_to_array(database_desire_next_year, E';'), ARRAY['Unset']::text[])  as databases_wanted
 from survey_2019_responses
-)
+),
 
-select * from responses_2016
-union all
+all_responses as (
 select * from responses_2017
 union all
 select * from responses_2018
 union all
 select * from responses_2019
+)
+
+select 
+    *,
+    case
+        when year=2017 and titles && ARRAY['Datascientist', 'Machinelearningspecialist'] then true
+        when year=2018 and titles && ARRAY['Data or business analyst', 'Data scientist or machine learning specialist'] then true
+        when year=2019 and titles && ARRAY['Data scientist or machine learning specialist', 'Data or business analyst', 'Engineer, data'] then true
+        else false
+    end as has_analytics_title
+from all_responses
